2f0fdc59ef977622d8979346fcc571933d19f8ae
Georman <georman.calderon@cleteci.com>
2023-01-21 18:26:12 -0400

    configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+import awsExports from './aws-exports.jsm';
 Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
