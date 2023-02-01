bec507b4c915791cb1f6c3861d38bd864e4afc54
Georman <georman.calderon@cleteci.com>
2023-02-01 08:54:35 -0400

    textos in spanish

--- src/partials/Cta.jsx
+++ src/partials/Cta.jsx
@@ -20,7 +20,7 @@ function Cta() {
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center md:justify-start space-y-4 sm:space-y-0 sm:space-x-4 mb-12 md:mb-0">
                 <div>
                   <a className="btn-sm w-full inline-flex items-center text-blue-50 bg-blue-500 hover:bg-blue-600 group shadow-sm" href="apply.html">
-                    Get your card
+                    Descarga la App
                     <span className="tracking-normal text-sky-400 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-2">
                       <svg className="fill-current" width="12" height="10" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 6.002h7.586L6.293 8.295a1 1 0 1 0 1.414 1.414l4-4a1 1 0 0 0 0-1.416l-4-4a1 1 0 0 0-1.414 1.416l2.293 2.293H1a1 1 0 1 0 0 2Z" />
--- src/partials/Hero.jsx
+++ src/partials/Hero.jsx
@@ -47,7 +47,7 @@ function Hero() {
               >
                 <div>
                   <Link className="btn-sm w-full inline-flex items-center text-slate-100 bg-slate-800 hover:bg-slate-900 group shadow-sm" to="/apply">
-                    Descarga la app
+                  Descarga la aplicación
                     <span className="tracking-normal text-sky-400 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-2">
                       <svg className="fill-current" width="12" height="10" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 6.002h7.586L6.293 8.295a1 1 0 1 0 1.414 1.414l4-4a1 1 0 0 0 0-1.416l-4-4a1 1 0 0 0-1.414 1.416l2.293 2.293H1a1 1 0 1 0 0 2Z" />
